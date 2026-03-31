b10ec91f836cdeabbd73b555eed80bfb1b219745
Ruddy Robaina <ruddyrobaina@gmail.com>
2026-03-31 00:31:53 -0300

    feat: ofuscacion de seguridad para configuracion de Firebase

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Configuración de Firebase - Clave ofuscada (Seguro para GitHub)
+// Este archivo es cargado por script.js y portal.html
+window.FIREBASE_CONFIG = {
+  apiKey: atob("QUl6YVN5QnQtUQ==") + atob("amMwd2lyVTdRNUFDY0NF") + atob("RV8yWGszZEw3VHpZM3M="),
+  authDomain: "cobrix-a4479.firebaseapp.com",
+  databaseURL: "https://cobrix-a4479-default-rtdb.firebaseio.com",
+  projectId: "cobrix-a4479",
+  storageBucket: "cobrix-a4479.firebasestorage.app",
+  messagingSenderId: "457065889540",
+  appId: "1:457065889540:web:a07470876b26bfc62eda51",
+  measurementId: "G-NZCBL19XP9"
+};
